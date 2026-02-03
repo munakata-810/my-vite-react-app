118e7bdabae600d327e55ca2d9e915ad13950269
munakata-810 <munakata.hayato@icloud.com>
2026-02-03 18:35:55 +0900

    カウンター作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,39 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count,setCount] = useState(0)
 
+  //増やす処理
+  const increment = ()=> {
+    setCount(count+1)
+  }
+
+  //減らす処理
+  const decrement = () => {
+    setCount(count -1 )
+  }
+
+  //リセット処理
+  const reset = () => {
+    setCount(0)
+  }
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="container">
+    <h1>カウンター</h1>
+    <p>{count}</p>
+
+    <div>
+      <button onClick={increment} style={{color:"green"}}>増やす</button>
+    </div>
+
+    <div>
+      <button onClick={decrement} style={{color:"red"}}>減らす</button>
+    </div>
+
+    <div>
+      <button onClick={reset} style={{color:"blue"}}>リセット</button>
+    </div>
+  </div>
   )
 }
 
